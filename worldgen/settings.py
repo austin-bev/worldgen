58da0daa9e78e317c15a5db660d10c970d730159
Austin Bevacqua <62879045+austin-bev@users.noreply.github.com>
2023-04-24 11:06:54 +0800

    update settings

--- worldgen/settings.py
+++ worldgen/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t!jk)7pgmcsf9ulf_r6x$*c3f2n^31tla5x-1!=*s%rt(48tcq'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
